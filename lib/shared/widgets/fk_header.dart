04e6a526fd4f863d064224d80e8b71c547747570
drishtant-ranjan-landmark <drishtant@stylishop.com>
2026-08-08 10:59:25 +0530

    FKP-91 #comment Refractoring done on the basic code

--- lib/shared/widgets/fk_header.dart
+++ lib/shared/widgets/fk_header.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_knp_mobile_app_v2/app/theme/app_colors.dart';
 import 'package:flutter_knp_mobile_app_v2/app/theme/app_spacing.dart';
+import 'package:flutter_knp_mobile_app_v2/app/theme/app_text_styles.dart';
 
 class FkHeader extends StatelessWidget {
   const FkHeader({
     super.key,
     required this.title,
-    required this.subtitle,
+    this.subtitle,
     this.leading,
     this.trailing,
   });
 
   final String title;
-  final String subtitle;
+  final String? subtitle;
   final Widget? leading;
   final Widget? trailing;
 
   @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
+  Widget build(BuildContext context) {;
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -30,18 +29,12 @@ class FkHeader extends StatelessWidget {
             children: [
               Text(
                 title,
-                style: theme.textTheme.headlineSmall?.copyWith(
-                  fontWeight: FontWeight.w800,
-                  color: AppColors.blackBase,
-                ),
+                style: AppTextStyles.titleLarge
               ),
-              SizedBox(height: AppSpacing.v6),
+
               Text(
-                subtitle,
-                style: theme.textTheme.bodyMedium?.copyWith(
-                  color: AppColors.neutral500,
-                  height: 1.35,
-                ),
+                subtitle ?? "",
+                style: AppTextStyles.titleMedium
               ),
             ],
           ),
